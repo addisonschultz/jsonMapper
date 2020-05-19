c33e0a9134b19ff48e644cd508a6c9a030ddc614
Addison <addison@framer.com>
2020-05-19 21:10:40 +0200

    Add readme

--- merge.js
+++ merge.js
@@ -3,47 +3,13 @@ var fs = require("fs"),
 
 function reducer(jsonFile) {
   Object.keys(jsonFile).map(function (key, index) {
-    console.log(jsonFile[key]);
     return {
-      // labelInfo: {
-      //   labelAddress: "Koninginneweg 56-4",
-      //   labelPhone: "+31657811633",
-      //   labelImage: "japan_001.jpg",
-      //   labelDescription: "A description",
-      //   labelEmail: "addisonschultz0@gmail.com",
-      // },
-      products: [
-        {
-          color: jsonFile[key].color,
-          sex: jsonFile[key].sex,
-          // productImages: [
-          //   {
-          //     productImage: "japan_002.jpg",
-          //   },
-          // ],
-          price: jsonFile[key].price,
-          productDescription: jsonFile[key].productShortDescription,
-          optionalInfo: {
-            material: jsonFile[key].material,
-            fit: jsonFile[key].fit,
-            care: jsonFile[key].care,
-            production: jsonFile[key].production,
-            sku: jsonFile[key].sku,
-            additionalNotes: jsonFile[key].additionalNotes,
-          },
-          shipping: jsonFile[key].shipping,
-          productName: jsonFile[key].productName,
-          // inventory: [
-          //   {
-          //     size: "Medium",
-          //     quantity: 3,
-          //   },
-          // ],
-          sustainable: jsonFile[key].sustainable,
-          category: jsonFile[key].category,
-        },
-      ],
-      // labelName: "Creators Club",
+      /**
+       *
+       * I'll map over the data and replace it to a new
+       * object structure here, I can do this part
+       *
+       */
     };
   });
 }
---
+++ readme.txt
@@ -0,0 +1,6 @@
+I want to be able to run:
+
+
+`bash merge.sh input`
+
+where input is a directory of json files, and it will output a directory (output) of mapped json files. 
